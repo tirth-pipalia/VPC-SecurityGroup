14f2fc53f138d9097d85ad4628c38ed41f3a45c2
tirth pipalia <56732761+tirth-pipalia@users.noreply.github.com>
2022-04-04 17:30:35 +0100

    Update app.py

--- rms-vpc/app.py
+++ rms-vpc/app.py
@@ -7,7 +7,7 @@ from rms_vpc.rms_vpc_stack import RmsVpcStack
 
 
 app = cdk.App()
-env_ME = cdk.Environment(account="620228650709", region="me-south-1")
+env_ME = cdk.Environment(account="<12_digit_account_no>", region="me-south-1")
 RmsVpcStack(app, "RmsVpcStack",env=env_ME)
 
 app.synth()
